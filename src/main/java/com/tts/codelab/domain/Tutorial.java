617c063db290d174fae703127ec8d969e24c71a6
Thien Tran <thientran1986@gmail.com>
2017-02-14 16:37:09 +0700

    Add autogenerate for ID field

--- src/main/java/com/tts/codelab/domain/Tutorial.java
+++ src/main/java/com/tts/codelab/domain/Tutorial.java
@@ -1,13 +1,28 @@
 package com.tts.codelab.domain;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "tutorials")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Tutorial {
+    
+    @Transient
+    private static final AtomicInteger counter = new AtomicInteger(0);
+
+    @Transient
+    public static int nextValue() {
+        return counter.getAndIncrement();
+    }
+    
+    @Id
+    private Integer id = nextValue();
+    
     private String title;
 
     private Category category;
@@ -36,5 +51,13 @@ public class Tutorial {
 
     public void setSteps(List<TutorialStep> steps) {
         this.steps = steps;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 }
